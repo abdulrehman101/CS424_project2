09435ba6068595e9fb89bfc3a10ebb4abc4d97bc
jkarime <jaoudat104karime@gmail.com>
2020-03-01 15:46:40 -0600

    Version 0.2 added the date input

--- app.R
+++ app.R
@@ -11,7 +11,7 @@ library(shiny)
 library(shinydashboard) #To allow the making of UI Dashboard
 library(shinyWidgets)
 
-ui <- fluidPage(
+ui <- dashboardPage(
     
     dashboardHeader(title = "Project 2"),
     
@@ -22,21 +22,25 @@ ui <- fluidPage(
             pickerInput(
                 inputId = "yearSelect",
                 label = "Select/Deselect the Years 05-18",
-                choices = "2005", #insert the choices array here
+                choices = c("Morning ","Afternoon ","Evening ","Night ","default "), #insert the choices array here
                 options = list(
                     'actions-box' = TRUE,
                     size = 10,
-                    'selected-text-format' = "count > 3"
+                    'selected-text-format' = 'count > 3'
                     
                 ),#End of List
                 multiple = TRUE
             ),#End of pickerInput for yearSelection
             
+            #Choosing a specific day to look at hurricanes, POSSIBLY CHANGE FORMAT
+            #Min and max values correspond to the range and value is the starting date
+            dateInput(inputId = "dateSelect", label = "Date:", min = "1851-06-25",max = "2018-11-14", value = "2018-11-14", format = "mm/dd/yy"),
+        
             
             pickerInput(
                 inputId = "hurrSelect",
                 label = "Select/Deselect the Hurricane",
-                choices = "Abdul", #insert the choices array here
+                choices = c("Morning","Afternoon","Evening","Night","default"), #insert the choices array here
                 options = list(
                     'actions-box' = TRUE,
                     size = 10,
@@ -45,6 +49,8 @@ ui <- fluidPage(
                 ),#End of List
                 multiple = TRUE
             )#End of pickerInput for HurricaneSelection
+            
+            
             
             
             
@@ -58,7 +64,13 @@ ui <- fluidPage(
     ),#end of dashboardSideBar body
     
     dashboardBody(
-        
+        fluidRow(
+            
+                   box(title = "About : Total", solidHeader = TRUE, status = "primary", height = 500,
+                       textOutput("var")
+                   ) 
+            
+        ),
         
     ),
     
@@ -68,16 +80,17 @@ ui <- fluidPage(
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output, session) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+    #To update the hurricane names in the drop down boxes
+    updatePickerInput(session = session, inputId = "hurrSelect", choices = "Update") #Change the choices to update 
+    
+    
+    
+    
+    
+    
+    
 }
 
 # Run the application 
